24e94e0811d292ebf734b3ac168dd03917aa16d8
Abdulrahman-msk <abdulrahmanknakria@gmail.com>
2025-02-14 18:14:37 -0500

    times

--- assets/js/script.js
+++ assets/js/script.js
@@ -257,7 +257,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 "Dhuhr": "12:45 PM",
                 "Asr": "3:30 PM",
                 "Maghrib": maghribIqamaTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-                "Isha": "7:10 PM"
+                "Isha": "7:20 PM"
             };
 
             let now = new Date();
